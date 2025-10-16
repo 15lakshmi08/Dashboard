8b172933b0a27a32ebbcbb6649a44ff09679a308
15lakshmi08 <lakshmi150803@gmail.com>
2025-10-16 10:45:59 +0530

    msg

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import './App.css'
-import Sales from "./components/MainComp/Sales/Sales"
-import Visitor from "./components/MainComp/Visitor/Visitor"
-import Order from "./components/MainComp/Order/Order"
+import Sales from "./components/MainContent/Sales/Sales"
+import Visitor from "./components/MainContent/Visitor/Visitor"
+import Order from "./components/MainContent/Order/Order"
 import Footer from "./components/Footer/Footer"
-import Dashboard from "./components/MainComp/Dashboard/Card"
+import Dashboard from "./components/MainContent/Dashboard/Card"
 import Sidebar from "./components/Sidebar/Sidebar"
-import Income from  "./components/MainComp/Income/Income"
-import Transaction from "./components/MainComp/Transaction/Transaction"
+import Income from  "./components/MainContent/Income/Income"
+import Transaction from "./components/MainContent/Transaction/Transaction"
 
 function App() {
 
   return (
     <>
-
-        <Sidebar/>
+    <h1>Hello</h1>
+        {/* <Sidebar/>
         <Dashboard/>
         <Income/>
         <Transaction/>
         <Visitor/>
         <Order />
         <Sales />
-        <Footer/>
+        <Footer/> */}
     </>
   )
 }
